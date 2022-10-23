5b321d35ee480636f13fe18c75b2c356483629c7
furkaner23 <97389419+furkaner23@users.noreply.github.com>
2022-10-23 10:59:27 +0300

    -@Parameter annotation is used to combine negative username and negative password tests.

--- src/test/java/com/example/selenium/NegativeTests.java
+++ src/test/java/com/example/selenium/NegativeTests.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class NegativeTests {
 
+  @Parameters({"username", "password", "expectedMessage"})
   @Test(priority = 2, groups = {"negativeTests"})
-  public void incorrectUserNameTest() {
+  public void incorrectUserNameTest(String username, String password, String expectedMessage) {
 
     System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
     WebDriver webDriver = new ChromeDriver();
@@ -20,52 +22,52 @@ public class NegativeTests {
     String url = "http://the-internet.herokuapp.com/login";
     webDriver.get(url);
 
-    WebElement userName = webDriver.findElement(By.xpath("//*[@id=\"username\"]"));
-    userName.sendKeys("wrongUserName");
+    WebElement userNameElement = webDriver.findElement(By.xpath("//*[@id=\"username\"]"));
+    userNameElement.sendKeys(username);
 
-    WebElement password = webDriver.findElement(By.xpath("//*[@id=\"password\"]"));
-    password.sendKeys("SuperSecretPassword!");
-
-    WebElement loginButton = webDriver.findElement(By.xpath("//*[@id=\"login\"]/button/i"));
-    loginButton.click();
-
-    WebElement failMessage = webDriver.findElement(By.xpath("//*[@id=\"flash\"]"));
-
-    String expectedMessage = "Your username is invalid!";
-    String actualMessage = failMessage.getText();
-
-    Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain the expected message.");
-
-    webDriver.quit();
-  }
-
-  @Test(priority = 1, groups = {"negativeTests", "smokeTests"})
-  public void incorrectPasswordTest() {
-
-    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-    WebDriver webDriver = new ChromeDriver();
-
-    webDriver.manage().window().maximize();
-
-    String url = "http://the-internet.herokuapp.com/login";
-    webDriver.get(url);
-
-    WebElement userName = webDriver.findElement(By.xpath("//*[@id=\"username\"]"));
-    userName.sendKeys("tomsmith");
-
-    WebElement password = webDriver.findElement(By.xpath("//*[@id=\"password\"]"));
-    password.sendKeys("wrongPassword");
+    WebElement passwordElement = webDriver.findElement(By.xpath("//*[@id=\"password\"]"));
+    passwordElement.sendKeys(password);
 
     WebElement loginButton = webDriver.findElement(By.xpath("//*[@id=\"login\"]/button/i"));
     loginButton.click();
 
     WebElement failMessage = webDriver.findElement(By.xpath("//*[@id=\"flash\"]"));
 
-    String expectedMessage = "Your password is invalid!";
     String actualMessage = failMessage.getText();
 
     Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain the expected message.");
 
     webDriver.quit();
   }
+//  There is no need to have two separate username and password method as we can do it with @Parameter annotation.
+
+//  @Test(priority = 1, groups = {"negativeTests", "smokeTests"})
+//  public void incorrectPasswordTest() {
+//
+//    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+//    WebDriver webDriver = new ChromeDriver();
+//
+//    webDriver.manage().window().maximize();
+//
+//    String url = "http://the-internet.herokuapp.com/login";
+//    webDriver.get(url);
+//
+//    WebElement userName = webDriver.findElement(By.xpath("//*[@id=\"username\"]"));
+//    userName.sendKeys("tomsmith");
+//
+//    WebElement password = webDriver.findElement(By.xpath("//*[@id=\"password\"]"));
+//    password.sendKeys("wrongPassword");
+//
+//    WebElement loginButton = webDriver.findElement(By.xpath("//*[@id=\"login\"]/button/i"));
+//    loginButton.click();
+//
+//    WebElement failMessage = webDriver.findElement(By.xpath("//*[@id=\"flash\"]"));
+//
+//    String expectedMessage = "Your password is invalid!";
+//    String actualMessage = failMessage.getText();
+//
+//    Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain the expected message.");
+//
+//    webDriver.quit();
+//  }
 }
